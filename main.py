53253bbce80af15d758e885fc50ee4dea3c81636
dashulik <daryna.havrada@gmail.com>
2024-10-09 12:03:17 +0300

    break

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ Instrumentator().instrument(app).expose(app)
 
 @app.get("/products/{productId}")
 def get_product(productId: int):
+    a ="123123"
+    b = "123123"
+    c = "123123"
+    d = "123123"
     return {"id": str(productId), "name": f"{productId} name"}
 @app.get("/external-api")
 def call_external_api():
