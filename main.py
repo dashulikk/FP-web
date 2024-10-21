af0951001ca676566b298ada0d85c0cb28e4683c
dashulik <daryna.havrada@gmail.com>
2024-10-21 19:55:59 +0300

    Lab3 modifications

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
+from prometheus_fastapi_instrumentator import Instrumentator
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/products/{productId}")
 def get_product(productId: int):
-    a ="123123"
-    b = "123123"
-    c = "123123"
-    d = "123123"
     return {"id": str(productId), "name": f"{productId} name"}
-
-# comment for changes
-# test1
-# texst
+@app.get("/external-api")
+def call_external_api():
+    response = requests.get("https://api.exchangerate-api.com/v4/latest/USD")
+    return response.json()
