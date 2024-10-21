9991a4303b8f28fbed44e7546f747220f84dac3e
dashulik <daryna.havrada@gmail.com>
2024-10-21 20:47:15 +0300

    database added

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import requests
+import psycopg2
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname="web_app_db", user="postgres", password="postgres", host="localhost"
+    )
+    return conn
 
 app = FastAPI()
 from prometheus_fastapi_instrumentator import Instrumentator
@@ -12,3 +19,19 @@ def get_product(productId: int):
 def call_external_api():
     response = requests.get("https://api.exchangerate-api.com/v4/latest/USD")
     return response.json()
+
+@app.get("/users/")
+def get_users():
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, name, email FROM users")
+        users = cursor.fetchall()
+        cursor.close()
+        conn.close()
+
+        return [{"id": row[0], "name": row[1], "email": row[2]} for row in users]
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
